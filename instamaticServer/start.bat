1eaed58f07973ccc1d955a36fd37931561c0d97e
Daniel Tchoń <dantch@onet.eu>
2025-01-14 18:55:52 +0100

    Exit command is unnecessary and hides errors if there are any

--- instamaticServer/start.bat
+++ instamaticServer/start.bat
@@ -1,4 +1,3 @@
 cd c:\instamatic\TEMserver          &:: point to your installation directory
 :: call .\venv\Scripts\activate.bat &:: uncomment and point to venv if using one
 py tem_server.py                    &:: use `py`, `py3`, or `python` as needed
-EXIT
